2616d1442b95aa27e259cb738068685897a3bdfc
Mark Hentges <hentg089@umn.edu>
2024-03-09 22:51:13 -0600

    weigths schema finished

--- Weights/WeightSchema.cpp
+++ Weights/WeightSchema.cpp
@@ -5,6 +5,7 @@
 #include "WeightSchema.h"
 #include <cmath>
 
+
 WeightSchema::WeightSchema(int weightCount, double a, double r) {
     this->weightCount = weightCount;
     this->a = a;
@@ -12,22 +13,43 @@ WeightSchema::WeightSchema(int weightCount, double a, double r) {
 }
 
 double WeightSchema::getIthWeight(int index) {
-    if(index == 1){
-        return a;
-    }
+
     //if n is ven then we are dealing with higher order belief
     if(weightCount % 2 == 0){
-        if(index == 2){
-            return a * r;
-        }
-        int temp = (index - 2)/2;
+        int temp = (weightCount - 2)/2;
         if (index > 2 + temp){
             index -= temp;
         }
+        double ans = a * pow(r, index - 1);
+        if (index > 2){
+            ans /= 2;
+        }
 
-        return a * pow(r, index - 1);
-
+        if(index == 2 + temp){
+            double add = pow(r, index);
+            if(index > 2){
+                add /= 2;
+            }
+            ans += add;
+        }
+        return ans;
     }else{
+        int temp = (weightCount - 1)/2;
+        if(index > 1 + temp){
+            index -= temp;
+        }
+        double ans = a * pow(r, index-1);
+        if(index > 1){
+            ans /= 2;
+        }
 
+        if(index == 1 + temp){
+            double add = pow(r, index);
+            if(index > 1){
+                add /= 2;
+            }
+            ans += add;
+        }
+        return ans;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include "Model.h"
+#include "Weights/WeightSchema.h"
 
 int main() {
-    std::vector<double> weights_first = {0.9,0.05,0.05};
-    std::vector<double> weights_higher = {0.9,0.05,0.04, 0.01};
-    std::unordered_map<std::string, FirstOrderDiscreteBelief>
-            firstOrderBeliefs = {{"B1" , FirstOrderDiscreteBelief(0.6)},
-                                                                 {"B2", FirstOrderDiscreteBelief(0.5)}};
-    std::unordered_map<std::string, double> influence = {{"1,2", 0.8}, {"2,1", 0.6}};
+//    std::vector<double> weights_first = {0.9,0.05,0.05};
+//    std::vector<double> weights_higher = {0.9,0.05,0.04, 0.01};
+//    std::unordered_map<std::string, FirstOrderDiscreteBelief>
+//            firstOrderBeliefs = {{"B1" , FirstOrderDiscreteBelief(0.6)},
+//                                                                 {"B2", FirstOrderDiscreteBelief(0.5)}};
+//    std::unordered_map<std::string, double> influence = {{"1,2", 0.8}, {"2,1", 0.6}};
+//
+//
+//    int x = 2;
+//    int n = 2;
+//    int agentCount = 2;
+//
+//
+//    Model p(agentCount, x, n, std::move(firstOrderBeliefs), std::move(influence),
+//            std::move(weights_first), std::move(weights_higher));
+//
+//    p.avgDifferenceGenSecond("BV",3,"1",3, "5");
+//
+    WeightSchema p(2, 0.9, 0.1);
+    std::cout << p.getIthWeight(1) << std::endl;
+    std::cout << p.getIthWeight(2) << std::endl;
 
 
-    int x = 2;
-    int n = 2;
-    int agentCount = 2;
 
 
-    Model p(agentCount, x, n, std::move(firstOrderBeliefs), std::move(influence),
-            std::move(weights_first), std::move(weights_higher));
 
-    p.avgDifferenceGenSecond("BV",3,"1",3, "5");
+
+
 
 
 
